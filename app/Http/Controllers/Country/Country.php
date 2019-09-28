8629308822c77109fdb3d23fc8caace7c410f53d
dickenzoh <dickenzoh@gmail.com>
2019-09-28 17:57:40 +0300

    initial commit

---
+++ app/Http/Controllers/Country/Country.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Country;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Models\CountryModel;
+use Validator;
+
+class Country extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(CountryModel::get(), 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => 'required|min:3',
+            'iso' => 'required|min:2|max:2',
+        ];
+        $validator = Validator::make($request->all(), $rules);
+        if($validator->fails()){
+            return response()->json($validator->errors(), 400);
+        }
+
+        $country = CountryModel::create($request->all());
+        return response()->json($country, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row" >
+        
+        <div class="col-3 p-5">
+             <img class="rounded-circle" src="/img/logo.png">
+        </div>
+
+        <div class="col-9 pt-5">
+            <div><h1>Productionden</h1></div>
+            <div class="d-flex" >
+               <div class ="pr-4"><strong> 153 </strong> post </div>
+               <div class ="pr-4"><strong> 23k </strong> followers </div>
+               <div class ="pr-4"><strong> 212 </strong> following</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
